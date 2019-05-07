32277e9f930d0a1d68e1500007fd1844ab3c416e
John Tronolone <johntronolone@Johns-MacBook-Pro.local>
2019-05-07 11:44:04 -0400

    created subband samples

--- musicandeng_hw5.m
+++ musicandeng_hw5.m
@@ -12,21 +12,40 @@ numFilts = 32;
 sampleIncr = 32; % increment by 32 each iteration
 numBuffers = floor((length(recording)-bufferLength)/sampleIncr)+1;
 
+%window = zeors(512, 1);
+hk = sin(pi*(0:511+1/2)/(2*512));
+
+% load C and D windows
+[C, D] = createCandD;
+
+% create sine window
+n = 64;
+sineWindow = sin(pi*(1:n)/n);
+
+subband_samples = zeros(numBuffers, n/2);
+
 
 for ii = 1:numBuffers % for each buffer
     
-    PCM_in = signal((ii-1)*(sampleIncr)+1:(ii-1)*sampleIncr+bufferLength);
+    X_FIFO = signal((ii-1)*(sampleIncr)+1:(ii-1)*sampleIncr+bufferLength);
     
+    Z_vector = X_FIFO.*C'; 
     
+    Y_vector = sum(reshape(Z_vector, [], bufferLength/n), 2);
     
-%      for jj = 1:numFilts
-%          
-%          jj
-%          
-%      end
-%     
-    
+    for m = 1:(n/2)
+        for k = 1:n
+            summand(k) = Y_vector(k).*sineWindow(1,k)'.*cos(pi/2/n*(2*(k-1)+1+n/2)*(2*(m-1)+1));
+        end
+        subband_samples(ii,m) = sum(summand);
+    end
 end
 
+
+
 % filter audio buffer with 32 bands
+
+
+
+
 
